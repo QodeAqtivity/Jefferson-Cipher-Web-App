1a3727d16102693ea37a8b6c11a1c7a9d30b5cc7
QodeAqtivity <150616872+QodeAqtivity@users.noreply.github.com>
2024-03-24 14:19:56 -0700

    Navbar.jsx
    Created links/href and imported component

---
+++ jefferson-cipher/src/Navbar.jsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    
+    return (
+        <nav className="navbar"> 
+            <h1> Qoding Aqtivity Jefferson Cipher </h1>
+            <div className="links">
+                <a href="/create">Create Jefferson Ciphers</a>
+                <a href="/crack">Crack Jefferson Ciphers</a>
+                <a href="/view">View Jefferson Ciphers</a>
+            </div>
+        </nav>
+        
+    )
+}
+
+export default Navbar;
