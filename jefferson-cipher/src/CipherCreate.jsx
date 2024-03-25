12e865d9f2bf1fb28f46405c764e8f291b502fa4
QodeAqtivity <150616872+QodeAqtivity@users.noreply.github.com>
2024-03-25 00:00:59 -0700

    Referencing TextArea Input

--- jefferson-cipher/src/CipherCreate.jsx
+++ jefferson-cipher/src/CipherCreate.jsx
@@ -1,4 +1,34 @@
+import { useState } from 'react'; 
+
 const CipherCreate = () => {
+
+    const [unencrypted, setUnencrypted] = useState('');
+    
+    const handleUnencryptedChange = (event) => {
+        console.log(`Test me (event target value): ${event.target.value}`);
+        setUnencrypted(event.target.value);
+        console.log(`Current unencrypted message is: ${unencrypted}`);
+    }
+
+    const randomGenerateWheelSet = () => {
+        console.log(`The unencrypted value is: ${unencrypted}`);
+
+        let wheel_set = [];
+
+        let message_index = Math.floor(Math.random() * 25); //26 letters but 25 index
+
+        for (let i = 0; i < unencrypted.length; i++){
+
+        }
+        
+    }
+
+    const clickSubmitHandler = (event) => {
+        event.preventDefault();
+        console.log(`Submitted unencrypted message is: ${unencrypted}`);
+        randomGenerateWheelSet()
+    }
+
 
 
     return ( 
@@ -9,8 +39,9 @@ const CipherCreate = () => {
             <div className="message-create">
                 <form>
                     <label for="message">Message</label>
-                    <input type="text" id="message"></input>
-                    <button onClick={clickHandler}>Submit</button>
+                    {/* <input type="text" id="message"></input> */}
+                    <textarea value={unencrypted} onChange={handleUnencryptedChange} id="message" name="message" required></textarea>
+                    <button onClick={clickSubmitHandler}>Submit</button>
                     <br />
                     <input type="radio" id="Self-Generate" name="testname"></input> 
                     <label for='Self-Generate'>Self Generate Wheel Set</label>
