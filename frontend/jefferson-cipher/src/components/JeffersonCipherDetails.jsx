a17fb3dfb9ab33867b3cc2ed3ce19ec0051dfc30
QodeAqtivity <150616872+QodeAqtivity@users.noreply.github.com>
2024-03-30 00:58:08 -0700

    Update JeffersonCipherDetails.jsx
    Implemented Buttons for downloading text files of WheelSet, Delivery Combo, Solution Combo

--- frontend/jefferson-cipher/src/components/JeffersonCipherDetails.jsx
+++ frontend/jefferson-cipher/src/components/JeffersonCipherDetails.jsx
@@ -1,9 +1,70 @@
 
 const JeffersonCipherDetails = ({ jeffersonCipher }) => {
+
+    const handleDeliveryComboDownload = (event) => {
+        event.preventDefault();
+    
+        const blob = new Blob([jeffersonCipher.deliveryCombo], { type: 'text/plain'});
+
+        const url = URL.createObjectURL(blob);
+        
+        const link = document.createElement('a');
+
+        link.href = url;
+        link.download = 'Delivery Combo';
+
+        document.body.appendChild(link);
+        link.click();
+
+        URL.revokeObjectURL(url);
+        document.body.removeChild(link);
+    };
+
+    const handleSolutionComboDownload = (event) => {
+        event.preventDefault();
+    
+        const blob = new Blob([jeffersonCipher.solutionCombo], { type: 'text/plain'});
+
+        const url = URL.createObjectURL(blob);
+        
+        const link = document.createElement('a');
+
+        link.href = url;
+        link.download = 'Solution Combo';
+
+        document.body.appendChild(link);
+        link.click();
+
+        URL.revokeObjectURL(url);
+        document.body.removeChild(link);
+    };
+
+    const handleWheelSetDownload = (event) => {
+        event.preventDefault();
+
+        const blob = new Blob([jeffersonCipher.wheelSet], {type: 'text/plain'});
+
+        const url = URL.createObjectURL(blob);
+
+        const link = document.createElement('a');
+
+        link.href = url;
+        link.download = 'Wheel Set';
+
+        document.body.appendChild(link);
+        link.click();
+
+        URL.revokeObjectURL(url);
+        document.body.removeChild(link);
+    }
+
     return (
         <div className="jefferson-cipher-details">
             <h3>Encrypted String: {jeffersonCipher.encrypted.replaceAll(',', '')}</h3>
             <p><strong>Delivery Combo: </strong>{jeffersonCipher.deliveryCombo.replaceAll(',', ' ')}</p>
+            <button id="downloadWheelSet" onClick={(event) => handleWheelSetDownload(event)}>Download WheelSet</button>
+            <button id="downloadDeliveryCombo" onClick={(event) => handleDeliveryComboDownload(event)}>Download Delivery/Encrypted Combo</button>
+            <button id="downloadSolutionCombo" onClick={(event) => handleSolutionComboDownload(event)}>Download Solution Combo</button>
         </div>
     )
 }
