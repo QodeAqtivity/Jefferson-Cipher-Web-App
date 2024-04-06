1404fab0b39276098f42df3875efe17b67ab7307
QodeAqtivity <150616872+QodeAqtivity@users.noreply.github.com>
2024-04-06 12:38:45 -0700

    Update CaesarShift.js
    Removed commented ACII unfinished algorithm to clean up codebase

--- backend/cipher logic/CaesarShift.js
+++ backend/cipher logic/CaesarShift.js
@@ -5,7 +5,6 @@ const UPPERALPHAARR = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L
 const NUMERICARR = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 const caesarShiftEncryption = (unencrypted, shift) => {
-    console.log(typeof unencrypted);
     let encrypted = '';
 
     for (let i = 0; i < unencrypted.length; i++){
@@ -35,42 +34,3 @@ const caesarShiftEncryption = (unencrypted, shift) => {
 module.exports = {
     caesarShiftEncryption,
 };
-// may be slower using ascii code logic
-// const shiftEncrypting = (unencrypted, shift) => {
-//     //input validated at front-end
-//     let encrypted = '';
-//     for (let i = 0; i < unencrypted.length; i++){
-//         let curr_char = unencrypted[i];
-        
-//         if (curr_char !== ' '){
-//             let curr_char_code;
-//             if (regexes.onlyLowerAlphaAndSpaces(curr_char)){
-//                 if (unencrypted.charCodeAt(i) === 122){
-//                     curr_char_code = 97;
-//                 } else{
-//                     curr_char_code += shift;
-//                     curr_char_code += curr_char
-//                 }
-//             }
-//             else if (regexes.onlyUpperAlphaAndSpaces(curr_char)){
-//                 if (unencrypted.charCodeAt(i) === 90) {
-//                     curr_char_code = 65; 
-//                 } else {
-//                     curr_char_code++;
-//                 }
-//             }
-//             else if (regexes.onlyNumeric(curr_char)){
-//                 if (unencrypted.charCodeAt(i) === 57){
-//                     curr_char_code = 48;
-//                 } else {
-//                     curr_char_code++;
-//                 }
-//             }
-//             curr_char = String.fromCharCode(curr_char_code);
-//         }   
-
-//         encrypted += curr_char;
-//     }
-
-//     console.log('')
-// };
