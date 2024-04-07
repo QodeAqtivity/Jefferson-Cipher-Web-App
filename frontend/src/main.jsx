e926b6a032350423d4fe88d30d2498e43f6a6c62
QodeAqtivity <150616872+QodeAqtivity@users.noreply.github.com>
2024-04-07 16:19:19 -0700

    Update main.jsx
    Made App component take full viewport/screen height and width

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App class='h-screen w-screen'/>
   </React.StrictMode>,
 )
