6649a0cba66d4edb501e7a75891ea1d385db23ba
QodeAqtivity <150616872+QodeAqtivity@users.noreply.github.com>
2024-04-07 12:59:26 -0700

    Update App.jsx
    Replaced with App.js in CRA project dir

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from './Navbar';
+import CipherCreate from './CipherCreate';
+import Home from './pages/Home';
+import JeffersonCipher from './pages/JeffersonCipher';
+import VigenereCipher from './pages/VigenereCipher';
+import CaesarCipher from './pages/CaesarCipher';
+import About from './pages/About';
+import Ciphers from './pages/Ciphers';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route
+              path="/" //homepage route
+              element={<Home />}  
+            />
+            <Route 
+              path="/ciphers"
+              element={<Ciphers />}
+            />
+            <Route 
+              path="/ciphers/jefferson-cipher"
+              element={<JeffersonCipher />}
+            />
+            <Route
+                path="/ciphers/vigenere-cipher"
+                element={<VigenereCipher />}
+            />
+            <Route
+                path="/ciphers/caesar-cipher"
+                element={<CaesarCipher />}
+            />
+
+            <Route
+              path="/about"
+              element={<About />}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
