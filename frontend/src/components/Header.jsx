16b454f1692c743d2a7c538f158701b2059e326f
QodeAqtivity <150616872+QodeAqtivity@users.noreply.github.com>
2024-04-07 16:17:27 -0700

    Created and Styled Header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+
+    return(
+        <div className="header" class='flex justify-around font-bold text-5xl'>
+            <h1>QODING AQTIVITY'S CIPHERS</h1>
+        </div>
+    )
+}
+
+export default Header;
