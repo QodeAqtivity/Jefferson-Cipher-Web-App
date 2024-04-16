12289acffee25db3f2bdd0767bdd06fa0f4344f9
QodeAqtivity <150616872+QodeAqtivity@users.noreply.github.com>
2024-04-16 00:28:33 -0700

    fix: Header now links and styled

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,17 @@
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Header = () => {
-
+    const donate_img_paths = ['./../public/donate_cat.png', './../public/donate_nocat.png'];
+    // let donate_img = './../public/donate_nocat.png';
+    const [donateImg, setDonateImg] = useState('./../public/donate_nocat.png');
     return(
-        <div className="header" class='flex justify-around font-bold text-5xl'>
-            <h1>QODING AQTIVITY'S CIPHERS</h1>
+        <div
+         className="header" class='flex justify-around font-bold text-5xl py-5'>
+            <Link to='/'>
+                <h1>QODING AQTIVITY'S CIPHERS</h1>
+            </Link>
+            
         </div>
     )
 }
