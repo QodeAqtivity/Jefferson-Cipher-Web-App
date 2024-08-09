b54467218cc79ffbb1bc6a6d017531c7b1d052fe
QodeAqtivity <150616872+QodeAqtivity@users.noreply.github.com>
2024-08-08 21:45:33 -0700

    Update main.jsx
    Added CaesarCipherContextProvider import as comment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+// import { CaesarCiphersContextProvider } from './context/CaesarCiphersContext.jsx'
 // import { JeffersonCiphersContextProvider } from './context/JeffersonCipherContext.jsx';
 import { AuthContextProvider } from './context/AuthContext.jsx';
 
