f11b81957eaa058a49b5f3eac336f53e74231ff3
QodeAqtivity <150616872+QodeAqtivity@users.noreply.github.com>
2024-08-10 12:14:26 -0700

    feat: implemented backend authorization middleware to protect routes

---
+++ backend/middleware/requireAuth.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
+
+const requireAuth = async (req, res, next) => { 
+
+    // verify authentication
+    const { authorization } = req.headers
+
+    console.log('authorization: ', authorization);
+
+    if (!authorization) {  //ensure that the request had an authorization (JWT) token with it
+        console.log('protector')
+        return res.status(401).json({error: 'Authorization (JWT) token required'});
+    };
+
+    //now ensure it is a valid JWT
+    // authorization will look like: Bearer tokenpt1.tokenpt2.tokenpt3
+
+    const token = authorization.split(' ')[1];
+
+    try {
+        const { _id } = jwt.verify(token, process.env.SECRET); //returns payload from that token, specifically destructure to find _id
+        console.log('check for me: ', _id);
+        req.user = await User.findOne({ _id }).select('_id'); //this authentication middleware function runs first and attaches the user property for next middleware functions
+        // for clarification we are appending/attaching a user property.
+        next();
+    } catch (error) {
+        res.status(401).json({error: 'Request is not authorized (Invalid JWT)'});
+    }
+};
+
+module.exports = requireAuth;
