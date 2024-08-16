20ea8b1559a6337389f4d818f78233f0e6337b51
QodeAqtivity <150616872+QodeAqtivity@users.noreply.github.com>
2024-08-16 13:23:20 -0700

    feat: added userID field to 'user' in localStorage on Login and Signup
    allows us to determine ownership of ciphers without constant api calls to backend server

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -17,7 +17,9 @@ const loginUser = async(req, res) => {
 
         const token = createJWToken(user._id);
 
-        res.status(200).json({email, token});
+        const userID = user._id;
+
+        res.status(200).json({email, token, userID});
     } catch (error) {
         res.status(400).json({error: error.message});
     }
@@ -32,7 +34,9 @@ const signupUser = async(req, res) => {
 
         const token = createJWToken(user._id);
 
-        res.status(200).json({email, token})
+        const userID = user._id;
+
+        res.status(200).json({email, token, userID});
     } catch (error) {
         res.status(400).json({error: error.message});
     };
